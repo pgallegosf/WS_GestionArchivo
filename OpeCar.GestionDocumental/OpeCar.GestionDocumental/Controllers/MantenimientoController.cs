2eacbfce878474cb030d778f64ad79888945357b
EXJSANC8 - LaptopSider <jeanscastro7@gmail.com>
2019-12-19 16:25:25 -0500

    Mantenimiento de información de la organizacion
    En pruebas..

---
+++ OpeCar.GestionDocumental/OpeCar.GestionDocumental/Controllers/MantenimientoController.cs
@@ -0,0 +1,41 @@
+﻿using OpeCar.GestionDocumental.Models.Entities;
+using OpeCar.GestionDocumental.Models.Infrastructure.Repositories;
+using System.Web.Http;
+
+namespace OpeCar.GestionDocumental.Controllers
+{
+    public class MantenimientoController : ApiController
+    {
+        [HttpGet]
+        [Route("api/Mantenimiento/Listar")]
+        public IHttpActionResult Listar()
+        {
+            var resul = DMantenimiento.Listar();
+            return Ok(resul);
+        }
+
+        [HttpGet]
+        [Route("api/Mantenimiento/ListarMenuFooter/{mantenimientoId}")]
+        public IHttpActionResult ListarMenuFooter(int mantenimientoId)
+        {
+            var resul = DMenuFooter.Listar(mantenimientoId);
+            return Ok(resul);
+        }
+
+        [HttpPost]
+        [Route("api/Mantenimiento/Guardar")]
+        public IHttpActionResult Guardar(EMantenimiento mantenimiento)
+        {
+            var resul = DMantenimiento.Guardar(mantenimiento);
+            return Ok(resul);
+        }
+
+        [HttpPost]
+        [Route("api/Mantenimiento/GuardarMenuFooter")]
+        public IHttpActionResult GuardarMenuFooter(EMenuFooter menuFooter)
+        {
+            var resul = DMenuFooter.Guardar(menuFooter);
+            return Ok(resul);
+        }
+    }
+}
---
+++ OpeCar.GestionDocumental/OpeCar.GestionDocumental/Models/Infrastructure/Repositories/DMenuFooter.cs
@@ -0,0 +1,100 @@
+﻿using OpeCar.GestionDocumental.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OpeCar.GestionDocumental.Models.Infrastructure.Repositories
+{
+    public class DMenuFooter
+    {
+        public static IEnumerable<EMenuFooter> Listar(int mantenimientoId)
+        {
+            var lista = new List<EMenuFooter>();
+            using (var db = new OpeCarEntities())
+            {
+                try
+                {
+                    var query = (from m in db.MenuFooter
+                                 where (m.MantenimientoId == mantenimientoId)
+                                 orderby m.MenuFooterPosition ascending
+                                 select new
+                                 {
+                                     m.MenuFooterId,
+                                     m.MantenimientoId,
+                                     m.MenuFooterName,
+                                     m.MenuFooterUrl,
+                                     m.MenuFooterPosition,
+                                     m.MenuFooterIsSuperAdmin,
+                                     m.MenuFooterStatus
+                                 });
+
+                    {
+                        foreach (var item in query)
+                        {
+                            var result = new EMenuFooter
+                            {
+                                MenuFooterId = item.MenuFooterId,
+                                MantenimientoId = item.MantenimientoId,
+                                MenuFooterName = item.MenuFooterName,
+                                MenuFooterUrl = item.MenuFooterUrl,
+                                MenuFooterPosition = item.MenuFooterPosition,
+                                MenuFooterIsSuperAdmin = item.MenuFooterIsSuperAdmin,
+                                MenuFooterStatus = item.MenuFooterStatus
+                            };
+                            lista.Add(result);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    throw ex;
+                }
+
+            }
+            return lista.OrderBy(x => x.MantenimientoId);
+        }
+
+        public static bool Guardar(EMenuFooter request)
+        {
+            using (var db = new OpeCarEntities())
+            {
+                try
+                {
+                    if (request.MenuFooterId != null)
+                    {                       
+                        var menuFooter = db.MenuFooter.FirstOrDefault(x => x.MenuFooterId == request.MenuFooterId);
+
+                        if (menuFooter != null)
+                        {
+                            menuFooter.MenuFooterName = request.MenuFooterName;
+                            menuFooter.MenuFooterUrl = request.MenuFooterUrl;
+                            menuFooter.MenuFooterPosition = request.MenuFooterPosition;
+                            menuFooter.MenuFooterStatus = request.MenuFooterStatus;
+                            menuFooter.MenuFooterIsSuperAdmin = request.MenuFooterIsSuperAdmin;
+                        }
+                    }
+                    else
+                    {                        
+                        var menuFooterNew = new MenuFooter
+                        {
+                            MantenimientoId = request.MantenimientoId,
+                            MenuFooterName = request.MenuFooterName,
+                            MenuFooterUrl = request.MenuFooterUrl,
+                            MenuFooterPosition = request.MenuFooterPosition,
+                            MenuFooterStatus = request.MenuFooterStatus,
+                            MenuFooterIsSuperAdmin = request.MenuFooterIsSuperAdmin
+                        };
+                        db.MenuFooter.Add(menuFooterNew);                       
+                    }
+                    db.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
